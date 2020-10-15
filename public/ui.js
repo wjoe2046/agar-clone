e1518fd1cf6253c7a92a28bd8c4ba0e997e2d668
Wenfei Zhou <wzhou2046@gmail.com>
2020-10-15 14:42:06 -0400

    added jquery to load the first page

--- public/ui.js
+++ public/ui.js
@@ -1,5 +1,31 @@
+// const { init } = require('../express/expressMain');
+
 let wHeight = $(window).height();
 let wWidth = $(window).width();
+let player = {}; //this is all thigns this player
 
 let canvas = document.querySelector('#the-canvas');
 let context = canvas.getContext('2d');
+
+canvas.width = wWidth;
+canvas.height = wHeight;
+
+$(window).load(() => {
+  $('#loginModal').modal('show');
+});
+
+$('.name-form').submit((event) => {
+  event.preventDefault();
+  //   console.log('Submitted!');
+  player.name = document.querySelector('#name-input').value;
+  $('#loginModal').modal('hide');
+  $('#spawnModal').modal('show');
+
+  document.querySelector('.player-name').innerHTML = player.name;
+});
+
+$('.start-game').click((event) => {
+  $('.modal').modal('hide');
+  $('.hiddenOnStart').removeAttr('hidden');
+  //   init();
+});
