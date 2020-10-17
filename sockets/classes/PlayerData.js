ffe2a22013bb0334694c65aa57c729d4d796e45d
Wenfei Zhou <wzhou2046@gmail.com>
2020-10-17 14:30:45 -0400

    added object factory for player configuration

--- sockets/classes/PlayerData.js
+++ sockets/classes/PlayerData.js
@@ -5,5 +5,14 @@ class PlayerData {
     this.locX = Math.floor(settings.worldWidth * Math.random() + 100);
     this.locY = Math.floor(settings.worldHeight * Math.random() + 100);
     this.radius = settings.defaultSize;
+    this.color = this.getRandomColor();
+    this.score = 0;
+  }
+
+  getRandomColor() {
+    const r = Math.floor(Math.random() * 200 + 50);
+    const g = Math.floor(Math.random() * 200 + 50);
+    const b = Math.floor(Math.random() * 200 + 50);
+    return `rgb(${r}, ${g}, ${b})`;
   }
 }
