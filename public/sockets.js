8529dccf09e190f3797315efc63fdcdf43a2faa8
Wenfei Zhou <wzhou2046@gmail.com>
2020-10-16 20:35:10 -0400

    push orbs to arraay function

---
+++ public/sockets.js
@@ -0,0 +1,6 @@
+let socket = io.connect('http://localhost:8080');
+
+socket.on('init', (data) => {
+  console.log('initiated');
+  console.log(data);
+});
--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -1,4 +1,20 @@
 //where all our main sockets stuff will go
 const io = require('../servers').io;
 
+const Orb = require('./classes/Orb');
+let orbs = [];
+
+initGame();
+
+io.sockets.on('connect', (socket) => {
+  socket.emit('init', { orbs });
+});
+
+//populate the game canvas with orbs
+function initGame() {
+  for (let i = 0; i < 500; i++) {
+    orbs.push(new Orb());
+  }
+}
+
 module.exports = io;
